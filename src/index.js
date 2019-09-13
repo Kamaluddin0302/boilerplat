3592b7394901050186c7612431e2ff01bcd807dd
Kamaluddin0302 <kamaluldin1999@gmail.com>
2019-09-13 10:07:48 +0500

    ecommerce-web-ap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+
+// import 'mdbreact/dist/css/mdb.css';
+// import "./assets/scss/mdb.scss"
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
